3d4362abddd6ec4de4779a1e2333e9642e979787
lutyputy <andrew99**@@>
2019-03-11 20:08:34 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -9,19 +9,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <errno.h>
-
-typedef struct http_header {
-    char *name;
-    char *value;
-} http_header_t;
-
-typedef struct http_request {
-    char *verb;
-    char *path;
-    char *version;
-    int num_headers;
-    http_header_t headers[MAX_HEADERS];
-} http_request_t;
+#include "httpv.h"
 
 // Returns 1 on success,
 // -1 on invalid HTTP request,
--- Programs/Prog2/httpv.h
+++ Programs/Prog2/httpv.h
@@ -7,6 +7,19 @@
 
 #define MAX_HEADERS 10
 
+typedef struct http_header {
+    char *name;
+    char *value;
+} http_header_t;
+
+typedef struct http_request {
+    char *verb;
+    char *path;
+    char *version;
+    int num_headers;
+    http_header_t headers[MAX_HEADERS];
+} http_request_t;
+
 // Returns 1 on success,
 // -1 on invalid HTTP request,
 // -2 on I/O error,
