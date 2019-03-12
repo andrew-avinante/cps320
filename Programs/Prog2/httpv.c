b8fc18f7939705f87fe549b88dbf08851e01c766
lutyputy <andrew99**@@>
2019-03-11 21:41:56 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -139,7 +139,7 @@ int generateResponse(int result, http_request_t *request, FILE *out)
     ssize_t recd;
     FILE *fstream;
     printf("%d\n", result);
-    if(request != NULL)
+    if(result == 1)
     {
         fstream = fopen(&request->path[1], "r+");
         if(fstream == NULL) { result = -5; }
