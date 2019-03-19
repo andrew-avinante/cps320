87f89eb80136faacb3826103773ad7d996a5e7a7
lutyputy <andrew99**@@>
2019-03-19 18:50:06 -0400

    Push

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -87,6 +87,7 @@ int parseHttp(FILE *in, http_request_t **request)
         rc = -7;
         goto cleanup;
     }
+    printf("TEST");
     if(strstr(req->version, "\r\n") == 0)
     {
         rc = -1;
