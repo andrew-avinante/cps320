f2dbafafa86c028298d6803c5f26212802f0c57e
lutyputy <andrew99**@@>
2019-03-19 20:39:51 -0400

    Push

--- Programs/Prog2/utils.h
+++ Programs/Prog2/utils.h
@@ -1,3 +1,8 @@
-#include <string.h>     // Standard string functions
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include "httpv.h"
 
 void blog(const char *fmt, ...);
