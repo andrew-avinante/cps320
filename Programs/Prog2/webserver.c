72a6537b5baa1718240422a9b8eec9a89e279f95
lutyputy <andrew99**@@>
2019-03-27 16:45:03 -0400

    Program 2

--- Programs/Prog2/webserver.c
+++ Programs/Prog2/webserver.c
@@ -164,12 +164,7 @@ int main(int argc, char **argv) {
     server_running = true;
     while (server_running) {
         struct client_info client;
-            if(connectedCount >= g_settings.socketNum)
-            {
-                destroy_client_info(&client);
-                blog("Max connections reached! %d/%d current connections.", connectedCount, g_settings.socketNum);
-                continue;
-            }
+
         // Wait for a connection on that socket
         if (wait_for_client(server_sock, &client)) {
             // Check to make sure our "failure" wasn't due to
@@ -177,6 +172,13 @@ int main(int argc, char **argv) {
             // it was  "real" error, report it, but keep serving.
             if (errno != EINTR) { perror("unable to accept connection"); }
         } else {
+            if(connectedCount >= g_settings.socketNum)
+            {
+                destroy_client_info(&client);
+                blog("Max connections reached! %d/%d current connections.", connectedCount, g_settings.socketNum);
+                continue;
+            }
+
             child = fork();
             if(child == 0)
             {
