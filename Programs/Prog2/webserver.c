d0b9e4c75d39297c84099f75e64a3b67c7dd8df1
lutyputy <andrew99**@@>
2019-03-27 16:31:14 -0400

    Program 2

--- Programs/Prog2/webserver.c
+++ Programs/Prog2/webserver.c
@@ -173,7 +173,7 @@ int main(int argc, char **argv) {
         } else {
             if(connectedCount >= g_settings.socketNum)
             {
-                destroy_client_info(&client);
+                // destroy_client_info(&client);
             }
             child = fork();
             if(child == 0)
