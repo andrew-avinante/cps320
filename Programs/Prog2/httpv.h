a329b53f40b46557a7fb933a327f9d365b85f439
lutyputy <andrew99**@@>
2019-03-28 21:26:17 -0400

    Program 2

--- Programs/Prog2/httpv.h
+++ Programs/Prog2/httpv.h
@@ -22,7 +22,7 @@ typedef struct http_request {
 
 int verifyInput(http_request_t *req);
 
-int parseRequestLine(char *line, char *reqWord, char **save, len);
+int parseRequestLine(char *line, char *reqWord, char **save, size_t len);
 
 // Returns 1 on success,
 // -1 on invalid HTTP request,
