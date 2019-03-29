81b423e3719fe2b8a9f1c22312f7e37e160d8893
lutyputy <andrew99**@@>
2019-03-28 21:21:59 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -41,7 +41,7 @@ int verifyInput(http_request_t *req)
 }
 
 // This function parses a portion of the http request and stores it in the variable `reqWord`
-int parseRequestLine(char *line, char *reqWord, char **save, size_t len)
+int parseRequestLine(char *line, char *reqWord, char **save)
 {
     char *token = strtok_r(line, " ", save);
     if(token == NULL)
@@ -49,7 +49,7 @@ int parseRequestLine(char *line, char *reqWord, char **save, size_t len)
         return -2;
     }
     strlcpy(reqWord, token, len);    //Coppies token to VERB
-    reqWord[len - 1] = 0;            //Adds null terminator
+    reqWord[strlen(token) - 1] = 0;            //Adds null terminator
     return -1;
 }
 
@@ -104,9 +104,9 @@ int parseHttp(FILE *in, http_request_t **request)
     req->verb = malloc(VERB_SIZE); 
     req->path = malloc(PATH_SIZE); 
     req->version = malloc(VERSION_SIZE);
-    if((rc = parseRequestLine(line, req->verb, save, len)) != -1) goto cleanup;
-    if((rc = parseRequestLine(NULL, req->path, save, len)) != -1) goto cleanup;
-    if((rc = parseRequestLine(NULL, req->version, save, len)) != -1) goto cleanup;
+    if((rc = parseRequestLine(line, req->verb, save)) != -1) goto cleanup;
+    if((rc = parseRequestLine(NULL, req->path, save)) != -1) goto cleanup;
+    if((rc = parseRequestLine(NULL, req->version, save)) != -1) goto cleanup;
     
     if((rc = verifyInput(req)) != -1) goto cleanup;
 
--- Programs/Prog2/httpv.h
+++ Programs/Prog2/httpv.h
@@ -22,7 +22,7 @@ typedef struct http_request {
 
 int verifyInput(http_request_t *req);
 
-int parseRequestLine(char *line, char *reqWord, char **save, const int WORD_SIZE);
+int parseRequestLine(char *line, char *reqWord, char **save);
 
 // Returns 1 on success,
 // -1 on invalid HTTP request,
