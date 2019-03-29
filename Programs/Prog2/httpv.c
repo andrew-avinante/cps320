24166efb65239b28ae7ad6809546b868f149a3c4
lutyputy <andrew99**@@>
2019-03-29 18:50:20 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -109,10 +109,9 @@ int parseHttp(FILE *in, http_request_t **request)
     req->verb = malloc(VERB_SIZE); 
     req->path = malloc(PATH_SIZE); 
     req->version = malloc(VERSION_SIZE);
-      char *token = strtok_r(line, " ", save);
-    // if((rc = parseRequestLine(line, req->verb, save, VERB_SIZE)) != -1) goto cleanup;
-    // if((rc = parseRequestLine(NULL, req->path, save, PATH_SIZE)) != -1) goto cleanup;
-    // if((rc = parseRequestLine(NULL, req->version, save, VERSION_SIZE)) != -1) goto cleanup;
+    if((rc = parseRequestLine(line, req->verb, save, VERB_SIZE)) != -1) goto cleanup;
+    if((rc = parseRequestLine(NULL, req->path, save, PATH_SIZE)) != -1) goto cleanup;
+    if((rc = parseRequestLine(NULL, req->version, save, VERSION_SIZE)) != -1) goto cleanup;
     
     if((rc = verifyInput(req)) != -1) goto cleanup;
 
