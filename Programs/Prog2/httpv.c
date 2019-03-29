8f10d69a0de130667a5e9eaf5bb595192390019f
lutyputy <andrew99**@@>
2019-03-28 21:00:03 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -141,7 +141,6 @@ int generateResponse(int result, http_request_t *request, FILE *out)
         {
             fstream = fopen(&request->path[1], "r+");
             strtok_r(request->path, ".", &fileExt);
-            printf("%d\n", getDictLen(contentDict));
             for(int i = 0; i < DICT_SIZE; i++)
             {
                 if(strcmp(contentDict[i].key, fileExt) == 0)
