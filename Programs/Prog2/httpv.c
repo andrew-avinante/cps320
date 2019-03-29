c036f96cfad8a0564d21164fae568e759f7ffd9c
lutyputy <andrew99**@@>
2019-03-29 18:54:52 -0400

    Program 2

--- Programs/Prog2/httpv.c
+++ Programs/Prog2/httpv.c
@@ -40,7 +40,8 @@ int verifyInput(http_request_t *req)
 // This function parses a portion of the http request and stores it in the variable `reqWord`
 int parseRequestLine(char *line, char *reqWord, char **save, const int WORD_SIZE)
 {
-    char *token = NULL;
+    char *test = "Hello Bob";
+    char *token = strtok_r(test, " ", save);
     if(token == NULL)
     {
         return -2;
