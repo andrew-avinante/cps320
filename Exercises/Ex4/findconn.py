9763aa3df17e9c3b7f19cc4ebc44af7b6a07e510
lutyputy <andrewavinante@gmail.com>
2019-04-02 14:37:03 -0700

    Added wswars

--- Exercises/Ex4/findconn.py
+++ Exercises/Ex4/findconn.py
@@ -6,9 +6,12 @@ rdblah = ctypes.cdll.LoadLibrary("./readblah.so")
 
 def search(path : str) -> list:
     connections = []
-    # if('blah' in path):
-    #     f = rdblah.open_blah(path)
-    f = open(path, "r")
+    if('.blah' in path):
+        f = rdblah.open_blah(str.encode('UTF-8'))
+        # blahString = rdblah.read_blah(f)
+        rdblah.close_blah(f)
+    else:
+        f = open(path, "r")
     for i in f:
         match = re.findall(r"""(
             mysql:\/\/              # matches 'mysql//'
