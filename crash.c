5eeaf53043796d8eb21c4116fe1fb0944fa72efc
lutyputy <andrew99**@@>
2019-04-03 09:30:58 -0400

    Yay

--- crash.c
+++ crash.c
@@ -12,10 +12,10 @@ int main()
     fork();
     while(true)
     {
-        system("eho -en 'GET / HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50:8080 8080");
+        system("eho -en 'GET / HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080");
         system("echo -en 'MUSH\r\n\r\n' | nc -C 10.10.26.50:8080");
-        system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50:8080 8080");
-        system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50:8080 8080"); 
-        system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.50:8080 8080");
+        system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080");
+        system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080"); 
+        system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.50 8080");
     }
 }
