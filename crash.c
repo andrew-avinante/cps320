bb9ac998220dcc21eec2a02622216f3bc293c840
lutyputy <andrew99**@@>
2019-04-03 09:38:14 -0400

    Yay

--- crash.c
+++ crash.c
@@ -17,5 +17,21 @@ int main()
         system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080");
         system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080"); 
         system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.50 8080");
+
+         system("echo -en 'GET / HTTP/1.0\r\n\r\n' | nc -C 10.10.26.99 8080");
+        system("echo -en 'MUSH\r\n\r\n' | nc -C 10.10.26.99:8080");
+        system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.99 8080");
+        system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.99 8080"); 
+        system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.99 8080");
+         system("echo -en 'GET / HTTP/1.0\r\n\r\n' | nc -C 10.10.26.71 8080");
+        system("echo -en 'MUSH\r\n\r\n' | nc -C 10.10.26.71:8080");
+        system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.71 8080");
+        system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.71 8080"); 
+        system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.1 8080");
+         system("echo -en 'GET / HTTP/1.0\r\n\r\n' | nc -C 10.10.26.1 8080");
+        system("echo -en 'MUSH\r\n\r\n' | nc -C 10.10.26.50:8080");
+        system("echo -en 'MUSH /stuff HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080");
+        system("echo -en 'GET ../../ HTTP/1.0\r\n\r\n' | nc -C 10.10.26.50 8080"); 
+        system("echo -en 'GET / HTTP/1.0' | nc -C 10.10.26.50 8080");
     }
 }
