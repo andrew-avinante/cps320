9a1b56047c86eed722ae3578e3219c768c606162
lutyputy <andrewavinante@gmail.com>
2019-04-14 19:11:59 -0700

    TEST

--- Programs/Prog3/voip.py
+++ Programs/Prog3/voip.py
@@ -8,36 +8,45 @@ from threading import Thread
 from time import sleep
 
 class Broadcast(Thread):
-  def __init__(self, handle):
-    super().__init__()
-    self.handle = handle
-    
-  def run(self):
-    while True:
-        sock.sendto(b"HI", ('<broadcast>', 5000))
-        m=sock.recvfrom(1024)
-        print(m)
+    discovered = {}
+    def __init__(self, handle):
+        super().__init__()
+        self.handle = handle
+        
+    def run(self):
+        while True:
+            sock.sendto(handle.encode('UTF-8'), ('<broadcast>', PORT))
+            data, addr = sock.recvfrom(1024)
+            data = data.decode("UTF-8")
+            if data != handle:
+                discovered[data] = datetime.now()
+            time.sleep(1)
 
-
+class Display(Thread):
+    def __init__(self):
+        super().__init__()
+        
+    def run(self):
+        while True:
+            for i in Broadcast.discovered:
+                if datetime.now() - Broadcast.discovered[i] > 10000:
+                    del Broadcast.discovered[i]
+            print(Broadcast.discovered)
+            time.sleep(10)
+        
 
 PORT = 2000    # Port to transmit to
-
-start_time = datetime.now()
-
-# returns the elapsed milliseconds since the start of the program
-def millis():
-   dt = datetime.now() - start_time
-   ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-   return ms
 
 if len(sys.argv) != 2:
     print('Usage: python3 voip.py handle')
     sys.exit(1)
 
-handle = sys.argv[1]
+handle = sys.argv[1] + '@awaiting'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.bind(('',PORT))
+sock.bind(("", 2000))
 
 broadcast = Broadcast(handle)
+display = Display()
 broadcast.start()
+display.start()
