0e9242ace60497aa3151095fd95fa23db2b88c27
lutyputy <andrewavinante@gmail.com>
2019-04-21 19:02:43 -0700

    PUSH

--- Programs/Prog3/voip.py
+++ Programs/Prog3/voip.py
@@ -64,7 +64,7 @@ class Recieve(Thread):
                 Broadcast.partyHandle = ''
                 Broadcast.incomingRequest = False
 
-            if recieveAction == 'call' and Broadcast.action != '@call' and recieverHandle == handle and !incall:
+            if recieveAction == 'call' and Broadcast.action != '@call' and recieverHandle == handle and not incall:
                 Broadcast.incomingRequest = True
                 Broadcast.curAction = 'incoming'
                 Broadcast.partyHandle = senderHandle
