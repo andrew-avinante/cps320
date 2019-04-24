b676014b350b67b6bd8d64348ad156eb2957ebf3
lutyputy <andrewavinante@gmail.com>
2019-04-24 09:36:09 -0700

    Push

--- Programs/Prog3/voip.py
+++ Programs/Prog3/voip.py
@@ -111,7 +111,7 @@ class Recieve(Thread):
                 Broadcast.curAction = 'await'
             elif recieveAction[0] == 'incall':
                 Broadcast.curAction = 'incall'
-            print(Recieve.block + ' ' + senderHandle + ' ' handle)
+            print(Recieve.block + ' ' + senderHandle + ' ' + handle)
             if senderHandle != handle:
                 Broadcast.discovered[senderHandle] = [datetime.now(), senderData]
                 if senderHandle == Recieve.block:
