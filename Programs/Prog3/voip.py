18eaeab00342dd85e0e49fbf06c88bd3fdfb8847
lutyputy <andrewavinante@gmail.com>
2019-04-24 11:24:13 -0700

    Push

--- Programs/Prog3/voip.py
+++ Programs/Prog3/voip.py
@@ -85,32 +85,33 @@ class Recieve(Thread):
                 Broadcast.partyHandle = ''
                 Broadcast.incomingRequest = False
 
-            if recieveAction[0] == 'call' and Broadcast.action != '@call' and recieveAction[1] == handle and not Broadcast.incall and Recieve.block != senderHandle:
-                Broadcast.incomingRequest = True
-                Broadcast.curAction = 'incoming'
-                Broadcast.partyHandle = senderHandle
-                self.engine.say("Call from " + senderHandle)
-                self.engine.runAndWait()
-                Recieve.partyIP = recieveAction[2]
-            elif recieveAction[0] == 'reject' and recieveAction[1] == handle:
-                Broadcast.incomingRequest = False
-                Broadcast.deviceToCall = ''
-                Broadcast.curAction = 'await'
-                self.engine.say(senderHandle + " Declined your call")
-                self.engine.runAndWait()
-            elif recieveAction[0] == 'accept':
-                Broadcast.incomingRequest = False
-                Broadcast.incall = True
-                Broadcast.curAction = 'incall'
-                Broadcast.partyHandle = senderHandle
-                Recieve.partyIP = recieveAction[2]
-            elif senderData == 'endcall':
-                Broadcast.incall = False
-                Broadcast.partyHandle = ''
-                Recieve.partyIP = ''
-                Broadcast.curAction = 'await'
-            elif recieveAction[0] == 'incall':
-                Broadcast.curAction = 'incall'
+            if senderHandle != handle:
+                if recieveAction[0] == 'call' and Broadcast.action != '@call' and recieveAction[1] == handle and not Broadcast.incall and Recieve.block != senderHandle:
+                    Broadcast.incomingRequest = True
+                    Broadcast.curAction = 'incoming'
+                    Broadcast.partyHandle = senderHandle
+                    self.engine.say("Call from " + senderHandle)
+                    self.engine.runAndWait()
+                    Recieve.partyIP = recieveAction[2]
+                elif recieveAction[0] == 'reject' and recieveAction[1] == handle:
+                    Broadcast.incomingRequest = False
+                    Broadcast.deviceToCall = ''
+                    Broadcast.curAction = 'await'
+                    self.engine.say(senderHandle + " Declined your call")
+                    self.engine.runAndWait()
+                elif recieveAction[0] == 'accept':
+                    Broadcast.incomingRequest = False
+                    Broadcast.incall = True
+                    Broadcast.curAction = 'incall'
+                    Broadcast.partyHandle = senderHandle
+                    Recieve.partyIP = recieveAction[2]
+                elif senderData == 'endcall':
+                    Broadcast.incall = False
+                    Broadcast.partyHandle = ''
+                    Recieve.partyIP = ''
+                    Broadcast.curAction = 'await'
+                elif recieveAction[0] == 'incall':
+                    Broadcast.curAction = 'incall'
            
             if senderHandle != handle:
                 Broadcast.discovered[senderHandle] = [datetime.now(), senderData]
